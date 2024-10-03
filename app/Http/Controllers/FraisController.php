f08e85fd56343f1450c674cf1325d6076c6d2f30
Reark-smey <rearksmey.by@lmdsio.fr>
2024-10-03 16:55:59 +0200

    test 5

--- app/Http/Controllers/FraisController.php
+++ app/Http/Controllers/FraisController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\MonException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 use App\dao\ServiceFrais;
@@ -14,10 +15,22 @@ class FraisController extends Controller
             $id = Session::get('id');
             $servicesFrais = new ServiceFrais();
             $mesFrais = $servicesFrais->getFrais($id);
-            return view( '/vues/listeFrais'.$mesFrais ,compact( 'erreur'  ));
+            return view( '/vues/listeFrais' ,compact( 'mesFrais', 'erreur'  ));
         } catch (Exception $e) {
             $erreur = $e->getMessage();
             return view('vues/error', compact('erreur'));
         }
     }
+    public function updateFrais($id_frais) {
+        $erreur = "";
+        try {
+            $serviceFrais = new ServiceFrais();
+            $unFrais = $serviceFrais->getById($id_frais);
+            $titreVue = "Modification d'une fiche de frais";
+            return view('/vues/formFrais', compact('unFrais','titreVue' ,'erreur'));
+        }catch (Exception $e) {
+            throw new MonException($e->getMessage(), 5);
+        }
+    }
+
 }
